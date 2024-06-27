e57d92a938fda08bb6012b92f0524e7c13314bab
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2024-06-27 17:59:08 +0530

    Release: new changes (#43)
    * reformatted
    
    * added prabhupada commentry
    
    ---------
    
    Co-authored-by: Prashant Tripathi <Prashant.Tripathi@unilever.com>

---
+++ .temp_script/api2data.py
@@ -0,0 +1,31 @@
+import os
+import json
+from pathlib import Path
+
+source_directory = Path("./slok")
+target_directory = Path("../bhagavad-gita-data/slok")
+
+# Find all JSON files in the source directory and its subdirectories
+json_files = source_directory.rglob("*.json")
+for source_path in json_files:
+    chapter, slok = 0, 0
+    try:
+        # Read the JSON content from the source file
+        with open(source_path, "r", encoding="utf-8") as source_file:
+            data = json.load(source_file)
+        chapter = int(str(source_path).split("\\")[1])
+        slok = int(str(source_path).split("\\")[2])
+        target_path = Path(
+            f"../bhagavad-gita-data/slok/bhagavadgita_chapter_{chapter}_slok_{slok}.json"
+        )
+
+        # Create target directory if it does not exist
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+        # Write the JSON content to the target file
+        with open(target_path, "w", encoding="utf-8") as fp:
+            json.dump(data, fp, ensure_ascii=False, indent=4)
+
+    except Exception as e:
+        print(chapter, slok)
+        print(e)
---
+++ .temp_script/data2api.py
@@ -0,0 +1,43 @@
+import os
+import json
+from pathlib import Path
+
+source_directory = Path("../bhagavad-gita-data/slok")
+target_directory = Path("./slok")
+
+# Find all JSON files in the source directory and its subdirectories
+json_files = source_directory.rglob("*.json")
+for source_path in json_files:
+    chapter, slok = 0, 0
+    try:
+        # Read the JSON content from the source file
+        with open(source_path, "r", encoding="utf-8") as source_file:
+            prabhu = json.load(source_file).get("prabhu", False)
+
+        chapter = int(str(source_path.name).split(".")[0].split("_")[2])
+        slok = int(str(source_path.name).split(".")[0].split("_")[4])
+        target_path = Path(f"./slok/{chapter}/{slok}/index.json")
+
+        # Create target directory if it does not exist
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+        # Read the JSON content from the source file
+        with open(target_path, "r", encoding="utf-8") as source_file:
+            data = json.load(source_file)
+
+        if prabhu:
+            data["prabhu"] = prabhu
+            # Write the JSON content to the target file
+            with open(target_path, "w", encoding="utf-8") as target_file:
+                json.dump(
+                    data,
+                    target_file,
+                    ensure_ascii=False,
+                    indent=4,
+                )
+        else:
+            raise Exception("comentry not found : " + str(source_path))
+
+    except Exception as e:
+        print(chapter, slok)
+        print(e)
